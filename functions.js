615f48255dc9eac5302514afcaf46daea39f373b
Yaron Cohen <yaroncnk@gmail.com>
2016-10-05 19:20:26 +0000

    commit 2 - functions

--- functions.js
+++ functions.js
@@ -6,3 +6,43 @@ function sortingString(arrOfStrings){
    
     });
 }
+
+//function 2 - sorted array of objects
+
+function user(name, email){
+    this.name = name;
+    this.email = email;
+}
+
+var yaron = new user('Yaron', 'yaroncnk@gmail.com');
+var dan = new user('Dan' , 'danco@smile.net');
+var carolita = new user('Carolita' , 'carolita@gmail.com');
+
+var arrOfObjects = [yaron, dan, carolita];
+
+
+function sortingName(arrOfObjects){ 
+    arrOfObjects.sort(function(a,b) {
+        return b.name.length-a.name.length; 
+    }); 
+    return arrOfObjects;
+  }
+
+
+function sortingEmail(arrOfObjects){
+    arrOfObjects.sort(function(a,b) {
+        var nameA = a.name.toUpperCase();
+        var nameB = b.name.toUpperCase();
+          if (nameA < nameB) {
+            return -1;
+          }
+          else if (nameA > nameB) {
+              return 1;
+            } else {
+              return 0;
+            } 
+    });
+
+  return arrOfObjects; 
+}
+  
